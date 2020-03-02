9763eac851ee0fc2f023313d484c54a577b203fc
Guilherme <ggpasqualino@gmail.com>
2020-03-02 22:06:26 +0100

    Add file with GameServer

---
+++ lib/game_server.ex
@@ -0,0 +1,31 @@
+defmodule GameServer do
+  use GenServer
+
+  defmodule State do
+    defstruct [:game, :generate_next]
+  end
+
+  def start_link({game, generate_next}) do
+    GenServer.start_link(__MODULE__, %State{game: game, generate_next: generate_next})
+  end
+
+  @impl true
+  def init(state) do
+    schedule_tick()
+    {:ok, state}
+  end
+
+  @tick_period_in_ms 500
+  defp schedule_tick do
+    Process.send_after(self(), :tick, @tick_period_in_ms)
+  end
+
+  @impl true
+  def handle_info(:tick, %State{game: game, generate_next: generate_next} = state) do
+    new_generation = generate_next.(game)
+    GameRenderer.render(new_generation)
+
+    schedule_tick()
+    {:noreply, %{state | game: new_generation}}
+  end
+end
