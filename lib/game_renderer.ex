d5c2ce1262f16ce5c4ec931e587c4d4edc55c4ff
Guilherme <ggpasqualino@gmail.com>
2020-07-18 22:05:51 +0200

    Convert game renderer into a gen server

--- lib/game_renderer.ex
+++ lib/game_renderer.ex
@@ -1,17 +1,34 @@
 defmodule GameRenderer do
-  def render(game) do
-    game
-    |> grid()
-    |> IO.write()
+  use GenServer
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  @impl true
+  def init(state) do
+    {:ok, state}
+  end
+
+  def render(%{size: _size, alive_cells: _alive_cells} = game) do
+    GenServer.cast(__MODULE__, {:render_game, game})
+  end
+
+  @impl true
+  def handle_cast({:render_game, game}, state) do
+    do_render(game)
+    {:noreply, state}
+  end
+
+  defp do_render(game) do
+    grid = IO.ANSI.reset() <> IO.ANSI.clear() <> IO.ANSI.home() <> grid(game)
+    IO.write(grid)
   end
 
   defp grid(%{size: size, alive_cells: alive_cells}) do
-    lines =
-      0..(size - 1)
-      |> Enum.map(fn y -> line(y, size, alive_cells) end)
-      |> Enum.join("\n")
-
-    IO.ANSI.reset() <> IO.ANSI.clear() <> IO.ANSI.home() <> lines
+    0..(size - 1)
+    |> Enum.map(fn y -> line(y, size, alive_cells) end)
+    |> Enum.join("\n")
   end
 
   defp line(line, size, alive_cells) do
