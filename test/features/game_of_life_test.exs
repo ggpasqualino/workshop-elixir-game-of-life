1cc7bc772d47b0f14e342b19d2a62f7c9d51d9f3
Guilherme <ggpasqualino@gmail.com>
2020-11-04 07:03:07 +0000

    Add indexes to the board example

--- test/features/game_of_life_test.exs
+++ test/features/game_of_life_test.exs
@@ -3,9 +3,10 @@ defmodule Features.GameOfLifeTest do
 
   describe "Any live cell with fewer than two live neighbors" do
     test "dies, as if by underpopulation." do
-      # D D D
-      # D A D
-      # D D D
+      #   0 1 2
+      # 0 D D D
+      # 1 D A D
+      # 2 D D D
       current_generation = GameOfLife.new([{1, 1}])
 
       next_generation = GameOfLife.next_generation(current_generation)
@@ -17,9 +18,10 @@ defmodule Features.GameOfLifeTest do
   @tag :skip
   describe "Any live cell with two or three live neighbors" do
     test "lives on to the next generation" do
-      # D A D
-      # D A D
-      # D A D
+      #   0 1 2
+      # 0 D A D
+      # 1 D A D
+      # 2 D A D
       current_generation = GameOfLife.new([{1, 0}, {1, 1}, {1, 2}])
 
       next_generation = GameOfLife.next_generation(current_generation)
@@ -31,9 +33,10 @@ defmodule Features.GameOfLifeTest do
   @tag :skip
   describe "Any live cell with more than three live neighbors" do
     test "dies, as if by overpopulation." do
-      # D A D
-      # A A A
-      # D A D
+      #   0 1 2
+      # 0 D A D
+      # 1 A A A
+      # 2 D A D
       current_generation = GameOfLife.new([{1, 0}, {0, 1}, {1, 1}, {2, 1}, {1, 2}])
 
       next_generation = GameOfLife.next_generation(current_generation)
@@ -45,9 +48,10 @@ defmodule Features.GameOfLifeTest do
   @tag :skip
   describe "Any dead cell with exactly three live neighbors" do
     test "becomes a live cell, as if by reproduction." do
-      # A A A
-      # D D D
-      # D D D
+      #   0 1 2
+      # 0 A A A
+      # 1 D D D
+      # 2 D D D
       current_generation = GameOfLife.new([{0, 0}, {1, 0}, {2, 0}])
 
       next_generation = GameOfLife.next_generation(current_generation)
